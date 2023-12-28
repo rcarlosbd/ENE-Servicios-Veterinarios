85c2d5d3c2e0844d5a0246298e36c62c5f92254b
Roberto Batista <127065490+rcarlosbd@users.noreply.github.com>
2023-12-27 23:25:29 -0300

    vinculando con página de castración

--- script.js
+++ script.js
@@ -49,146 +49,37 @@ contactForm.addEventListener("reset", function () {
   window.location.href = "#inicio";
 });
 
-// Sección de turnos está comentada por la incorporación de asistente virtual
-// **** Nuevo código bloqueando horas
 
-/*const nombre = document.querySelector("#nombre");
-const mascota = document.querySelector("#mascota");
-const motivo = document.querySelector("#motivo");
-const fecha = document.querySelector("#fecha");
-const hora = document.querySelector("#hora");
-const btnEnviar = document.querySelector("#btnEnviar");
 
-// Obtiene la fecha y hora actual
-const fechaActual = new Date();
-let diaSemana = fechaActual.getDay(); // 0 = Domingo, 1 = Lunes, ..., 6 = Sábado
 
-// Define los horarios de atención de la veterinaria
-const horariosAtencion = {
-  0: [], // Domingo
-  1: ["11:00", "18:00"], // Lunes
-  2: ["09:00", "18:00"], // Martes
-  3: ["09:00", "18:00"], // Miércoles
-  4: ["09:00", "18:00"], // Jueves
-  5: ["09:00", "18:00"], // Viernes
-  6: ["09:00", "15:00"], // Sábado
-};
 
-// Establece la fecha mínima y máxima aceptada
-const fechaMinima = new Date(fechaActual);
-if (diaSemana === 0 || (diaSemana === 1 && fechaActual.getHours() >= 14)) {
-  fechaMinima.setDate(fechaMinima.getDate() + 1);
-}
-const fechaMaxima = new Date(fechaMinima);
-fechaMaxima.setDate(fechaMaxima.getDate() + 30);
+// Código de redirección de pagina a castración
 
-const fechaMinimaISO = fechaMinima.toISOString().split("T")[0];
-const fechaMaximaISO = fechaMaxima.toISOString().split("T")[0];
 
-fecha.setAttribute("min", fechaMinimaISO);
-fecha.setAttribute("max", fechaMaximaISO);
 
-// Genera las opciones de hora en base a los horarios de atención
-function generarOpcionesHorario() {
-  const horarioDia = horariosAtencion[diaSemana];
-  const horaInicio = parseInt(horarioDia[0].split(":")[0]);
-  const minutoInicio = parseInt(horarioDia[0].split(":")[1]);
-  const horaFin = parseInt(horarioDia[1].split(":")[0]);
-  const minutoFin = parseInt(horarioDia[1].split(":")[1]);
+ // Obtener referencias a elementos HTML
+ const modal = document.getElementById("myModal");
+ const castracionLink = document.getElementById("castracionLink");
 
-  let opciones = "";
-  let hora = horaInicio;
-  let minuto = minutoInicio;
+ // Función para mostrar el modal y redirigir después de un tiempo
+ function redirectToAnotherSite() {
+   // Mostrar el modal
+   modal.style.display = "block";
 
-  while (hora < horaFin || (hora === horaFin && minuto <= minutoFin)) {
-    let horaFormateada = hora.toString().padStart(2, "0");
-    let minutoFormateado = minuto.toString().padStart(2, "0");
-    opciones += `<option value="${horaFormateada}:${minutoFormateado}">${horaFormateada}:${minutoFormateado}</option>`;
+   // Simular una espera de 2 segundos (ajusta según sea necesario)
+   setTimeout(function () {
+     // Ocultar el modal
+     modal.style.display = "none";
 
-    minuto += 30;
-    if (minuto >= 60) {
-      minuto -= 60;
-      hora++;
-    }
-  }
+     // Redirigir al usuario a la URL específica
+     window.location.href = "https://rcarlosbd.github.io/Mascotas-Plus/";
+   }, 5000);
+ }
 
-  hora.innerHTML = opciones;
-}
+ // Agregar un evento al enlace para activar el modal y redirigir al usuario
+ castracionLink.addEventListener("click", function (event) {
+   event.preventDefault(); // Evitar la acción predeterminada del enlace
 
-// Actualiza las opciones de hora al cambiar la fecha
-fecha.addEventListener("change", () => {
-  diaSemana = new Date(fecha.value).getDay();
-  generarOpcionesHorario();
-});
-
-// Genera las opciones de hora al cargar la página
-generarOpcionesHorario();
-
-function enviar(event) {
-  event.preventDefault(); // Evita el envío del formulario
-
-  if (
-    nombre.value === "" ||
-    mascota.value === "" ||
-    motivo.value === "" ||
-    fecha.value === "" ||
-    hora.value === ""
-  ) {
-    // Si algún campo está vacío, muestra una alerta
-    alert("Por favor, completa todos los campos del formulario.");
-  } else {
-    const fechaSeleccionada = new Date(fecha.value);
-    const horaSeleccionada = hora.value.split(":");
-    const horaElegida = new Date(
-      fechaSeleccionada.getFullYear(),
-      fechaSeleccionada.getMonth(),
-      fechaSeleccionada.getDate(),
-      parseInt(horaSeleccionada[0]),
-      parseInt(horaSeleccionada[1])
-    );
-    const horaInicio = new Date(
-      fechaSeleccionada.getFullYear(),
-      fechaSeleccionada.getMonth(),
-      fechaSeleccionada.getDate(),
-      parseInt(horariosAtencion[diaSemana][0].split(":")[0]),
-      parseInt(horariosAtencion[diaSemana][0].split(":")[1])
-    );
-    const horaFin = new Date(
-      fechaSeleccionada.getFullYear(),
-      fechaSeleccionada.getMonth(),
-      fechaSeleccionada.getDate(),
-      parseInt(horariosAtencion[diaSemana][1].split(":")[0]),
-      parseInt(horariosAtencion[diaSemana][1].split(":")[1])
-    );
-
-    if (horaElegida < horaInicio || horaElegida > horaFin) {
-      // Si la hora seleccionada no está dentro del horario de atención, muestra una alerta
-      const mensajeAlerta = `Estimado usuario, le recordamos que el horario de atención de nuestra veterinaria es de:%0A%0A- Lunes: 11:00 a 18:00 horas%0A- Martes a Viernes: 09:00 a 18:00 horas%0A- Sábados: 09:00 a 15:00 horas%0A%0AMuchas gracias por preferirnos.`;
-      alert(decodeURIComponent(mensajeAlerta.replace(/%0A/g, "\n")));
-    } else {
-      const mensaje = `¡Hola! Me gustaría agendar una consulta veterinaria para mi mascota. A continuación, les envío los detalles correspondientes.%0A%0A- Nombre del dueño: ${nombre.value}%0A- Nombre de la mascota: ${mascota.value}%0A- Motivo de la consulta: ${motivo.value}%0A- Fecha preferida: ${fecha.value}%0A- Hora preferida: ${hora.value}%0A%0AAgradecería que confirmen el turno. ¡Muchas gracias!`;
-
-      const mensajeFormateado = mensaje
-        .replace(/%0A/g, "\n")
-        .replace(/%0A%0A-/g, "\n-");
-
-      const enlaceWhatsApp = `https://api.whatsapp.com/send?phone=5491122553332&text=${encodeURIComponent(
-        mensajeFormateado
-      )}`;
-
-      window.open(enlaceWhatsApp, "_blank"); // Abre el enlace de WhatsApp en una nueva pestaña
-
-      // Resetea los campos del formulario después de enviar el mensaje
-      nombre.value = "";
-      mascota.value = "";
-      motivo.value = "";
-      fecha.value = "";
-      hora.value = "";
-
-      // Redirige a la sección de inicio
-      window.location.href = "#inicio";
-    }
-  }
-}
-
-btnEnviar.addEventListener("click", enviar);*/
+   // Mostrar el modal y redirigir después de un tiempo
+   redirectToAnotherSite();
+ });
